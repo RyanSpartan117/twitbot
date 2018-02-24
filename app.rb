3a55b61d23d4bd095db8445c095e65e10e1883fa
Ryan C <ryan_chapman3@hotmail.co.uk>
2018-02-24 16:17:01 +0000

    sample app run

--- app.rb
+++ app.rb
@@ -2,7 +2,7 @@ require_relative 'lib/twitter_bot_service'
 
 
 twitterBot = TwitterBotService.new
-twitterBotResults = twitterBot.search_tweets('@thecalmzone @spartaglobal', 'recent', 3)
+twitterBotResults = twitterBot.search_tweets('recent', 3)
 # twitterBot.quote_tweet("@spartaglobal #SpeakOut4MentalHealth, #SpeakOutForMentalHealth", twitterBotResults[0])
 
 twitterBot.autogenerate_status_quote(twitterBotResults[0])
